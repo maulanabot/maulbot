821f3b51673d0d10aa1734288349d4c31eaa8a34
maulanabot <111231919+maulanabot@users.noreply.github.com>
2022-08-14 20:37:40 +0700

    Update and rename plugins/donasi.js to plugin/donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+╭─「 Donasi • Gopay & Dana 」
+│ •  [083874258794 Gopay]
+│ •  [082228214355 Dana]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
